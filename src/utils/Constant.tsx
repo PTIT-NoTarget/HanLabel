5b9f21e2a4221f9be8fad6deb7e65b541c00c010
PTIT-NoTarget <HanND.B21CN324@stu.ptit.edu.vn>
2024-06-11 17:22:57 +0700

    fix API

--- src/utils/Constant.tsx
+++ src/utils/Constant.tsx
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL = "http://api.notarget.id.vn";
 export const BOOK_TEMPLATE_ID = "1717960999042";
 export const NOTEBOOK_TEMPLATE_ID = "2";
